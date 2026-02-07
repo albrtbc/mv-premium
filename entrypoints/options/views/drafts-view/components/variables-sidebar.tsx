2273c5e45439ca2bdab15e4386a46158d92a68f3
Adan <adangg1988@gmail.com>
2026-02-07 21:55:32 +0100

    fix: add note property to category groups for TypeScript compatibility

--- entrypoints/options/views/drafts-view/components/variables-sidebar.tsx
+++ entrypoints/options/views/drafts-view/components/variables-sidebar.tsx
@@ -68,6 +68,7 @@ export function VariablesSidebar({
 			id: label,
 			label,
 			fields: groupFields,
+			note: undefined as string | undefined,
 		}))
 	}, [customGroups, filteredFields, variableFilter])
 
